1f7951c149aded903ca3228c2a96cae6c59adf86
netbrum <netbrum@tutanota.com>
2025-05-17 11:27:15 +0200

    feat: add clap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use clap::Parser;
 use color_eyre::eyre::Result;
 use inquire::Select;
 use ssh2_config::{ParseRule, SshConfig, SshParserResult};
-use std::env::{self, VarError};
+use std::env;
 use std::fmt::Display;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::PathBuf;
 use std::process::Command;
+
+#[derive(Parser)]
+#[command(version)]
+pub struct Args {
+    /// Use this config file
+    #[arg(short, long)]
+    config: Option<String>,
+}
 
 struct Host {
     name: String,
@@ -44,22 +53,26 @@ fn get_hosts(config: &SshConfig) -> Vec<Host> {
     hosts
 }
 
-fn get_config_path() -> Result<PathBuf, VarError> {
+fn get_config_file(args: Args) -> Result<File> {
     let home_dir = env::var("HOME")?;
     let ssh_config = home_dir + "/.ssh/config";
-    let path = PathBuf::from(&ssh_config);
+    let config = args.config.unwrap_or(ssh_config);
 
-    Ok(path)
+    let path = PathBuf::from(config);
+
+    Ok(File::open(path)?)
 }
 
-fn parse_config(config_file: PathBuf) -> SshParserResult<SshConfig> {
-    let mut reader = BufReader::new(File::open(config_file)?);
+fn parse_config(file: File) -> SshParserResult<SshConfig> {
+    let mut reader = BufReader::new(file);
     SshConfig::default().parse(&mut reader, ParseRule::STRICT)
 }
 
 fn main() -> Result<()> {
     color_eyre::install()?;
-    let config_path = get_config_path()?;
+    let args = Args::parse();
+
+    let config_path = get_config_file(args)?;
     let config = parse_config(config_path)?;
     let term = env::var("TERM")?;
 
